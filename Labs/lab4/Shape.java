862602be5a05864a3044289ffe16a42b5418d059
DASH <haseeb099m@gmail.com>
2026-02-17 14:51:11 +0500

    Merge main into member-feature and resolve conflict

--- Labs/lab4/Shape.java
+++ Labs/lab4/Shape.java
@@ -1,7 +1,7 @@
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "red";
+    private String color = "green";
 
     public Shape(int x, int y) {
         this.x = x;
